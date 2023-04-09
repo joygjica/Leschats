eb144c6616d0ddcf038c60b0b9845c62ce188088
jose jimenez <joygjica@gmail.com>
2023-04-09 17:41:30 -0300

    add button and cat img

---
+++ leschats.js
@@ -0,0 +1,8 @@
+console.log("Esta es una pagina de gatos")
+
+let elementocaja = document.getElementById("aqui-va-texto")
+const buttonclick = () => {
+
+    elementocaja.innerHTML += "Hiciste click en Miau :)"
+    console.log("hiciste click acá")
+}
